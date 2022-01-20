d5b0e7a5672edc21d588e8e8325a8a10102e8269
Walther Roa <roawalter95@gmail.com>
2022-01-20 14:05:25 -0500

    Correcciones

--- ejercicios5/codigo.js
+++ ejercicios5/codigo.js
@@ -1,8 +1,7 @@
-const contenedor = document.querySelector(".flex-container");
 
 
-function crearLlaves (nombre,modelo,precio){
-	img = "<img class='llave-img' src= '/Img/htdocs/../javascriptyunior/../llave.png' >";
+const crearLlaves  =(nombre,modelo,precio)=>{
+	img = "<img class='llave-img' src= '/Img/JavaScript-Junior/../llave.png' >";
 	nombre = `<h2> ${nombre} </h2>`;
 	modelo = `<h3> ${modelo} </h3>`;
 	precio = `<p> Precio: <b> ${precio}</b> </p>`;
@@ -15,17 +14,17 @@ let fragmento = document.createDocumentFragment();
 for (var i = 1; i <= 12; i++) {
 	let modeloRamdon= parseInt(Math.random()*1000);
 	let precioRamdon = parseInt(Math.random()*1000000);
-	let llave = crearLlaves(`llaves ${i}`,
-		`modelo: ${modeloRamdon}`, ` $${precioRamdon}`);
+	let llave = crearLlaves(`llave: ${i}`,
+		`Modelo: ${modeloRamdon}`, ` $${precioRamdon}`);
 	let div = document.createElement("DIV");
 	div.addEventListener("click",()=>{
 		document.querySelector(".key-data").value = `Modelo: 
 		${modeloRamdon}, Precio: $${precioRamdon}`;
-	});//endia la data al servidor
+	});
 	div.tabIndex = i;
 	div.classList.add(`item${i}`, "flex-item");
 	div.innerHTML = llave[0]+llave[1]+llave[2]+llave[3];
 	fragmento.appendChild(div);
 }
 
-contenedor.appendChild(fragmento);
+document.querySelector(".flex-container").appendChild(fragmento);
